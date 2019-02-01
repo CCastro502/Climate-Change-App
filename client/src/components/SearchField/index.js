0b09129760708ec7761d6e378df06bb71293d723
CCastro <chris502castro@gmail.com>
2019-02-01 11:05:00 -0700

    Fixed size of chart, I think, and fixed form field submit

--- client/src/components/SearchField/index.js
+++ client/src/components/SearchField/index.js
@@ -4,10 +4,9 @@ import "./style.css";
 function SearchField(props) {
     return (
         <div id="area">
-            <form>
+            <form onSubmitCapture={props.onSubmit}>
                 <label htmlFor="search-field">Search an area of the world: </label>
                 <input className="form-control mr-sm-6" id="search-field" name="search" value={props.value} onChange={props.handleInputChange} />
-                <button onClick={props.onSubmit}>Search</button>
             </form>
         </div>
     );
